bf2c3fe17d6617153755c757ed9138a967804791
Huzaifa-Islam <official.huzaifa.coder@gmail.com>
2025-02-15 12:16:19 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send('Hello World!');
+//app.use accepts all the HTTP methods
+
+app.get('/user/:userId', (req,res)=>{
+    console.log(req.params); // this is how we get the parameters from the URL
+    res.send({firstName:"John", lastName:"Doe"});
 })
 
-app.use("/test",(req,res)=>{
-    res.send('Hello World!');
-
+//http://localhost:3000/user?id=123
+app.get('/user', (req,res)=>{
+    console.log(req.query); // this is how we get the query params from the URL
+    res.send({firstName:"John", lastName:"Doe"});
 })
+
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
