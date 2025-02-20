84727544e8287468784b82fd172da53f32c02daf
Huzaifa-Islam <official.huzaifa.coder@gmail.com>
2025-02-20 17:17:56 +0530

    user Auth created

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const {adminAuth} = require("./middlewares/auth")
+const {userAuth} = require("./middlewares/auth")
 //Auth middleware for all requests GET, POST, etc for apis starting with /admin
 app.use("/admin", adminAuth)
 
@@ -11,8 +12,11 @@ app.get("/admin/getAllData",  (req, res, next) => {
 
 app.get("/admin/deleteUser", (req, res) => {
     res.send("Deleted a user");
-  
 });
+
+app.get("/user/getUserData", userAuth, (req,res,next)=>{
+    res.send("User data sent")
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,7 +2,7 @@ const adminAuth = (req,res,next)=>{
     const token = "xyz";
     const isAdminAuthorized = token === "xyz";
     if (isAdminAuthorized){
-        console.log("authorized")
+        console.log("admin authorized")
         next()
     }  
     else{
@@ -10,4 +10,16 @@ const adminAuth = (req,res,next)=>{
     }
 }
 
-module.exports = {adminAuth}
+const userAuth = (req,res,next)=>{
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if (isAdminAuthorized){
+        console.log("user authorized")
+        next()
+    }  
+    else{
+        res.status(401).send("unauthorized request");
+    }
+}
+
+module.exports = {adminAuth, userAuth}
